5735d04859cd73e02efae73f8d8b40c19618704b
Sara Ghaemi <ghaemi.sr@gmail.com>
2019-12-16 18:13:00 -0500

    Update Heap.py

--- PythonIntermediate/DataStructures/Heap.py
+++ PythonIntermediate/DataStructures/Heap.py
@@ -2,10 +2,9 @@ import math
 
 
 class MinHeap:
-    '''
+    """
     Child nodes are greater than or equal to parent nodes
-    '''
-
+    """
     def __init__(self):
         self.data = []
         self.leaves = 0
@@ -18,25 +17,25 @@ class MinHeap:
         self.data = self.data[:len(self.data) - 1]
         # perform bubble down, heap property
         current_index = 0
-        while (True):
+        while True:
             # compare node to children
             left = (current_index * 2) + 1
             right = (current_index * 2) + 2
             # get smaller child
             smallest_child = None
-            if (left > len(self.data) - 1):
+            if left > len(self.data) - 1:
                 # case no left node exists, then no right node exists, heap is satisfied
                 break
-            elif (right > len(self.data) - 1):
+            elif right > len(self.data) - 1:
                 # case no right node exists, then left node is smallest
                 smallest_child = left
-            elif (self.data[left] > self.data[right]):
+            elif self.data[left] > self.data[right]:
                 smallest_child = right
             else:
                 smallest_child = left
 
             # compare node to it's smallest child, and perform swap if needed
-            if (self.data[current_index] > self.data[smallest_child]):
+            if self.data[current_index] > self.data[smallest_child]:
                 # swap
                 self.data[current_index], self.data[smallest_child] = self.data[smallest_child], self.data[
                     current_index]
@@ -50,10 +49,10 @@ class MinHeap:
         self.data.append(value)
         index_inserted = len(self.data) - 1
         # fix heap property
-        while (index_inserted != 0):
+        while index_inserted != 0:
             # if the index is less than the parent, swap up
             parent = math.floor((index_inserted - 1) / 2)
-            if (index_inserted != 0 and self.data[parent] >= self.data[index_inserted]):
+            if index_inserted != 0 and self.data[parent] >= self.data[index_inserted]:
                 # swap
                 self.data[parent], self.data[index_inserted] = self.data[index_inserted], self.data[parent]
                 index_inserted = parent
@@ -62,7 +61,7 @@ class MinHeap:
 
     def heapify(self, l):
         for i in range(0, len(l)):
-            if (len(self.data) < 1):
+            if len(self.data) < 1:
                 self.data.append(l[i])
             else:
                 self.insert(l[i])
@@ -71,7 +70,7 @@ class MinHeap:
         nodes = len(self.data)
         node_max = math.pow(2, 0)
         i = 1
-        while (nodes > node_max):
+        while nodes > node_max:
             node_max += math.pow(2, i)
             i += 1
         return i
@@ -81,7 +80,7 @@ class MinHeap:
         max_nodes = math.pow(2, 0)
         print(nodes, max_nodes)
         i = 1
-        while (nodes > max_nodes):
+        while nodes > max_nodes:
             max_nodes += math.pow(2, i)
             i += 1
             print(nodes, max_nodes)
